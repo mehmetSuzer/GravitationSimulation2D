56ad5c988dae58ae07654112273f6e62c3655af1
mehmetSuzer <mehmet.suzer@boun.edu.tr>
2023-02-28 16:51:02 +0300

    fourteenth commit

--- app/src/main/java/com/example/gravitationsimulation2d/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/gravitationsimulation2d/viewmodel/HomeViewModel.kt
@@ -60,7 +60,7 @@ class HomeViewModel @Inject constructor(
         val date = String.format(
             "%04d-%02d-%02d",
             cal.get(Calendar.YEAR),
-            cal.get(Calendar.MONTH),
+            cal.get(Calendar.MONTH)+1, // January = 0, February = 1, ...
             cal.get(Calendar.DAY_OF_MONTH)
         )
         state = state.copy(
